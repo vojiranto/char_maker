5be1aaf34e41932dfac50f0f403bb1e70a5e6515
al.lastor <al.lastor@bk.ru>
2017-05-23 01:23:00 +0300

    -v

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 #!/usr/bin/lua5.2
 math.randomseed(os.time())
-dofile ("./src/patterns.lua")       -- шаблоны персонажей.
-dofile ("./src/random/elem.lua")    -- выбор случайного элемента
-dofile ("./src/fem_name_make.lua")  -- преобразование имён в женский род
-dofile ("./src/pat_operations.lua") -- операции над шаблонами.
+dofile ("./src/patterns/patterns.lua")      -- шаблоны персонажей.
+dofile ("./src/random/elem.lua")            -- выбор случайного элемента
+dofile ("./src/fem_name_make.lua")          -- преобразование имён в женский род
+dofile ("./src/patterns/operations.lua")    -- операции над шаблонами.
 
 print (get_mal_name())
 pat_print(mal_pat)
